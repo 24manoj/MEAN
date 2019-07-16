31cf6eb4ea2d755e48e1ffa34ed732f08e563939
24manoj <manoj.ks.24.mk@gmail.com>
2019-07-16 17:43:50 +0530

    commit

--- Algo/dayoftheweek.js
+++ Algo/dayoftheweek.js
@@ -2,8 +2,8 @@ var read=require('readline-sync');
 var d=read.questionInt("Enter Day ");
 var m=read.questionInt("Enter Month ");
 var y=read.questionInt("Enter year ");
-y0 = y-(14-m)/ 12;
-x = y0 + y0/4-y0/100 + y0/400
-m0 = m + 12 *((14- m) / 12) - 2;
-d0 = (d + x + 31*m0 / 12)% 7;
+y0 = parseInt(y-(14-m)/ 12);
+x = parseInt(y0 + (y0/4)-(y0/100) + (y0/400));
+m0 =parseInt( m +( 12 *(14- m) / 12)-2);
+d0 = parseInt((d + x + 31*(m0 / 12))% 7);
 console.log(parseInt(d0));

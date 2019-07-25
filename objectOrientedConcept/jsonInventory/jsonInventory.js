4f035d9ee5404a4386154cb8d1c428d796b29d6c
24manoj <manoj.ks.24.mk@gmail.com>
2019-07-25 16:43:39 +0530

    objectOriented JSON Program completed

---
+++ objectOrientedConcept/jsonInventory/jsonInventory.js
@@ -0,0 +1,17 @@
+/********************************************************************************************************************
+ * @Execution : default node : cmd> jsonInventory.js
+ * @Purpose : Read json files and perform operations. 
+ * @description : Create a JSON file having Inventory Details for Rice, Pulses and Wheats with properties name, weight, price per kg.
+ * @overview : dataStructure  
+ * @author : manoj kumar k s <manoj.ks.24.mk@gmail.com>
+ * @version : 1.0
+ * @since : 25-july-2019
+ *******************************************************************************************************************/
+
+try {
+    let obj = require('../jsonInventory/utility');
+    obj.readJSON();
+    obj.calculate();
+} catch (e) {
+    console.log(e);
+}
---
+++ objectOrientedConcept/jsonInventory/utility.js
@@ -0,0 +1,63 @@
+try {
+    let read = require('readline-sync');
+    let fs = require('fs');
+    let n, arr = [],
+        jsonCon;
+    //Reads JSON file
+    let json = fs.readFileSync('../jsonInventory/dataInventory.json');
+
+    let readJSON = () => {
+        //Convert JSON file to JavaScript format
+        if (isEmptyObject(json))
+            throw  "Json is empty";
+
+        jsonCon = JSON.parse(json);
+    }
+    let isEmptyObject = (obj) => {
+        return !Object.keys(obj).length;
+    }
+
+    /* Function calculates cost and weights of the inventory*/
+    let calculate = () => {
+        JSON.inventoryCost = 0;
+        jsonCon.forEach((ele) => {
+            ele.rice.forEach(value => {
+                let cost = value.price * value.weight;
+                JSON.inventoryCost += cost;
+                console.log("-----------------------------");
+                console.log("Rice name     ::", value.name);
+                console.log("Rice weight   ::", value.weight);
+                console.log("Rice cost     ::", value.price);
+                console.log("Inventory cost for  ", value.name, " ==>", cost);
+                console.log("-----------------------------");
+            });
+            ele.pulses.forEach(value => {
+                let cost = value.price * value.weight;
+                JSON.inventoryCost += cost;
+                console.log("-----------------------------");
+                console.log("pulses name     ::", value.name);
+                console.log("pulses weight   ::", value.weight);
+                console.log("pulses cost     ::", value.price);
+                console.log("Inventory cost for  ", value.name, " ==>", cost);
+                console.log("-----------------------------");
+            });
+            ele.wheat.forEach(value => {
+                let cost = value.price * value.weight;
+                JSON.inventoryCost += cost;
+                console.log("-----------------------------");
+                console.log("wheat name     ::", value.name);
+                console.log("wheat weight   ::", value.weight);
+                console.log("wheat cost     ::", value.price);
+                console.log("Inventory cost for  ", value.name, " ==>", cost);
+                console.log("-----------------------------");
+            });
+        });
+        console.log("Total inventoryCost is ", JSON.inventoryCost);
+    }
+    module.exports = {
+        readJSON,
+        calculate
+    };
+} catch (e) {
+    console.log(e);
+}
